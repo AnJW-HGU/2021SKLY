1b86d4571f5c44b3b2e24a326495f1f4d6b978f2
AnJW-HGU <22000404@handong.edu>
2021-11-18 16:56:03 +0900

    Setting the team project

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:skly/app.dart';
+
+void main() async {
+  runApp(MultiProvider(
+    providers: [],
+    child: App(),
+  ));
+}
---
+++ lib/post/board.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BoardPage extends StatelessWidget {
+  const BoardPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Container(),
+    ));
+  }
+}
