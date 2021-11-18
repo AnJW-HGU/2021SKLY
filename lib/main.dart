8da72b7533b6fe7c28f0b76f97d50907757ef85f
AnJW-HGU <22000404@handong.edu>
2021-11-18 17:23:42 +0900

    fix: Remove provider code in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:skly/app.dart';
 
-void main() async {
-  runApp(MultiProvider(
-    providers: [],
-    child: App(),
-  ));
+// void main() async {
+//   runApp(MultiProvider(
+//     providers: [],
+//     child: App(),
+//   ));
+// }
+
+void main() {
+  runApp(
+    App(),
+  );
 }
