8bffafce6f8d9ae642b68734cf342f5c4211c15d
21700340JuwonBaek <61731386+21700340JuwonBaek@users.noreply.github.com>
2021-11-21 04:59:24 +0900

    ADD: message model ADD message model

--- lib/model/message.dart
+++ lib/model/message.dart
@@ -1,12 +1,16 @@
+enum messageType { text, photo }
+
 class Message {
   final String content;
   final DateTime sentTime;
   final String name;
   final String uid;
+  final String type;
 
   Message(
       {required this.content,
       required this.sentTime,
       required this.name,
-      required this.uid});
+      required this.uid,
+      required this.type});
 }
