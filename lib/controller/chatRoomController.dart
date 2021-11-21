867ac75436778c42beeb7088b616659c4e26536c
21700340JuwonBaek <61731386+21700340JuwonBaek@users.noreply.github.com>
2021-11-21 18:07:33 +0900

    UPDATE: rename controller and add message listner Change the name since chatRoomController will be suit for its function. Add controller and get message in realtime

--- lib/controller/chatRoomController.dart
+++ lib/controller/chatRoomController.dart
@@ -4,24 +4,34 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:skly/model/message.dart';
 
-class GetMessage extends GetxController {
+class ChatRoomController extends GetxController {
   List<Message> messages = [];
-  GetMessage() {
+  String? currentRoomId;
+  ChatRoomController() {
     init();
   }
+
+  setCurrentRoodId(String id) {
+    currentRoomId = id;
+    update();
+  }
+
   init() async {
+    await Firebase.initializeApp();
     FirebaseFirestore.instance
         .collection('TestChatRoom')
+        .orderBy('sendTime', descending: true)
         .snapshots()
         .listen((snapshot) {
       messages.clear();
-      snapshot.docs.forEach((document) {
+      snapshot.docs.forEach((document) async {
         messages.add(Message(
             content: document.data()['content'],
             name: document.data()['name'],
-            sentTime: document.data()['sentTime'],
+            sentTime: document.data()['sendTime'],
             type: document.data()['type'],
-            uid: document.data()['uid']));
+            uid: document.data()['uid'],
+            photo: document.data()['photo']));
       });
       update();
     });
