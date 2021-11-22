7caf06caf694f358b1dff59549190cf5cb9a8a78
AnJW-HGU <22000404@handong.edu>
2021-11-22 10:40:51 +0900

    feat: Add timepicker in addPostPage

--- lib/post/board.dart
+++ lib/post/board.dart
@@ -33,6 +33,13 @@ class BoardPage extends StatelessWidget {
             expandedHeight: 200.0,
             flexibleSpace: Placeholder(),
             backgroundColor: colorScheme.primary,
+            // bottom: ListView(
+            //   scrollDirection: Axis.horizontal,
+            //   children: [
+            //     _buildCategory(context, '치킨'),
+            //     _buildCategory(context, '밥'),
+            //   ],
+            // ),
           ),
           StreamBuilder<List<Post>>(
               stream: _postsController.stream,
@@ -78,45 +85,65 @@ class BoardPage extends StatelessWidget {
     );
   }
 
+  Widget _buildCategory(BuildContext context, String category) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          radius: 50,
+          backgroundColor: colorScheme.surface,
+        ),
+        Text('$category', style: TextStyle(fontSize: 14, color: colorScheme.surface),),
+      ],
+    );
+  }
+
   Widget _buildPost(BuildContext context, Post post) {
     final colorScheme = Theme.of(context).colorScheme;
-    return Container(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //   children: [
-            //     Text("[${post.store}]"),
-            //     Text("${post.place}"),
-            //   ],
-            // ),
-            Text('[${post.store}]'),
-            Text('${post.place}'),
-            SizedBox(
-              height: 3,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '${post.content}',
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Spacer(),
-                Text(
-                    '${DateFormat('MM.dd kk:mm').format((post.writeTime)!.toDate())}'),
-                SizedBox(
-                  width: 10,
-                ),
-                if (post.people == 100) Text('없음'),
-                if (post.people != 100) Text('0/${post.people}'),
-                // 참여자 수 받기 (수정)
-              ],
-            ),
-          ],
+    return GestureDetector(
+      onTap: () {
+        print('dialog 띄우기');
+      },
+      behavior: HitTestBehavior.opaque,
+      child: Container(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   children: [
+              //     Text("[${post.store}]"),
+              //     Text("${post.place}"),
+              //   ],
+              // ),
+              Text('[${post.store}]'),
+              Text('${post.place}'),
+              SizedBox(
+                height: 3,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '${post.content}',
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Spacer(),
+                  Text(
+                      '${DateFormat('MM.dd kk:mm').format((post.closeTime)!.toDate())}'),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  if (post.people == 100) Text('없음'),
+                  if (post.people != 100) Text('0/${post.people}'),
+                  // 참여자 수 받기 (수정)
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -124,60 +151,66 @@ class BoardPage extends StatelessWidget {
 
   Widget _buildClosePost(BuildContext context, Post post) {
     final colorScheme = Theme.of(context).colorScheme;
-    return Container(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("[${post.store}]",
-                        style: TextStyle(color: colorScheme.primaryVariant)),
-                    Text("${post.place}",
-                        style: TextStyle(color: colorScheme.primaryVariant)),
-                  ],
-                ),
-                Icon(Icons.check_circle, color: colorScheme.primaryVariant),
-              ],
-            ),
-            SizedBox(
-              height: 3,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '${post.content}',
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(color: colorScheme.primaryVariant),
-                ),
-                Spacer(),
-                Text(
-                  '${DateFormat('MM.dd kk:mm').format((post.writeTime)!.toDate())}',
-                  style: TextStyle(color: colorScheme.primaryVariant),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                if (post.people == 100)
+    return GestureDetector(
+      onTap: () {
+        print('dialog 띄우기');
+      },
+      behavior: HitTestBehavior.opaque,
+      child: Container(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("[${post.store}]",
+                          style: TextStyle(color: colorScheme.primaryVariant)),
+                      Text("${post.place}",
+                          style: TextStyle(color: colorScheme.primaryVariant)),
+                    ],
+                  ),
+                  Icon(Icons.check_circle, color: colorScheme.primaryVariant),
+                ],
+              ),
+              SizedBox(
+                height: 3,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
                   Text(
-                    '없음',
+                    '${post.content}',
+                    overflow: TextOverflow.ellipsis,
                     style: TextStyle(color: colorScheme.primaryVariant),
                   ),
-                if (post.people != 100)
+                  Spacer(),
                   Text(
-                    '0/${post.people}',
+                    '${DateFormat('MM.dd kk:mm').format((post.closeTime)!.toDate())}',
                     style: TextStyle(color: colorScheme.primaryVariant),
                   ),
-                // 참여자 수 받기 (수정)
-              ],
-            ),
-          ],
+                  SizedBox(
+                    width: 10,
+                  ),
+                  if (post.people == 100)
+                    Text(
+                      '없음',
+                      style: TextStyle(color: colorScheme.primaryVariant),
+                    ),
+                  if (post.people != 100)
+                    Text(
+                      '0/${post.people}',
+                      style: TextStyle(color: colorScheme.primaryVariant),
+                    ),
+                  // 참여자 수 받기 (수정)
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
