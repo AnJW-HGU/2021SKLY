f243183f11840f92152098698ab46a8248009324
21700340JuwonBaek <61731386+21700340JuwonBaek@users.noreply.github.com>
2021-11-28 11:42:01 +0900

    UPDATE: Fixing error Detected ERROR about null value.

--- lib/chat/chatList.dart
+++ lib/chat/chatList.dart
@@ -1,6 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:skly/profile/myProfile.dart';
+import 'package:skly/controller/chatListController.dart';
+import 'package:skly/src/sklyTheme.dart';
+import 'package:skly/widget/currentChatTile.dart';
 
 class ChatListPage extends StatefulWidget {
   const ChatListPage({Key? key}) : super(key: key);
@@ -12,25 +18,19 @@ class ChatListPage extends StatefulWidget {
 class _ChatListPageState extends State<ChatListPage> {
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
     return Scaffold(
-        body: ListView(children: [
-      ElevatedButton(
-          onPressed: () {
-            Get.to(() => MyProfilePage());
-          },
-          child: Text('Go to Profile!'))
-      /*GetBuilder<Controller>(builder: (_) {
-        return Column(
-          children: [
-            Text('${_.count}'),
-            ElevatedButton(
-                onPressed: () {
-                  _.increment();
-                },
-                child: Text('Increase!'))
-          ],
-        );
-      }),*/
-    ]));
+      appBar: AppBar(
+        title: Center(child: Text('TestChatRoom')),
+        backgroundColor: colorScheme.primary,
+      ),
+      body: GetBuilder<ChatListController>(
+        init: ChatListController(),
+        builder: (_) {
+          return ListView(children: _.currentChat);
+        },
+      ),
+    );
   }
 }
