5acd00506292b85b7e995f89f9c8a3fa88c72b7d
21700340JuwonBaek <61731386+21700340JuwonBaek@users.noreply.github.com>
2021-12-06 15:40:39 +0900

    UPDATE: Get out from chatting room

--- lib/controller/chatListController.dart
+++ lib/controller/chatListController.dart
@@ -2,33 +2,49 @@ import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:skly/model/post.dart';
 import 'package:skly/widget/currentChatTile.dart';
 import 'package:skly/repository/userRepository.dart';
 import 'package:skly/model/user.dart' as UserModel;
 
 class ChatListController extends GetxController {
   List<CurrentChatTile> currentChat = [];
+  Post? currentPost;
   UserModel.User? curUser;
+  //final snapshot = FirebaseFirestore.instance.collection('Post').orderBy('closeTime', descending: true).snapshots();
+
   ChatListController() {
     init();
   }
 
+  setTile(int index) {
+    currentChat.removeAt(index);
+    update();
+  }
+
   init() async {
     await Firebase.initializeApp();
-    currentChat.clear();
     FirebaseFirestore.instance
         .collection('User')
         .doc(FirebaseAuth.instance.currentUser!.uid)
         .collection('joining')
         .snapshots()
-        .listen((sanpshot) {
-      print('Change Detected');
+        .listen((sanpshot) async {
+      print('Remove event Starting');
       currentChat.clear();
-      sanpshot.docs.forEach((document) {
-        DocumentReference current = document.get('joiningChat');
-        current.get().then((value) {
-          print(document.id);
-          currentChat.add(CurrentChatTile(value.get('store'), document.id));
+      int index = 0;
+      sanpshot.docs.forEach((document) async {
+        DocumentReference current = await document.get('joiningChat');
+        current.get().then((value) async {
+          List<dynamic> peopleJoin = await value.get('peopleJoin');
+          List<String> peopleJoinStr = peopleJoin.cast<String>();
+          currentPost = Post(
+              store: await value.get('store'),
+              id: await value.get('id'),
+              userId: await value.get('userId'),
+              peopleJoin: peopleJoinStr);
+          currentChat.add(CurrentChatTile(post: currentPost, index: index));
+          index += 1;
         });
       });
       update();
--- lib/widget/currentChatTile.dart
+++ lib/widget/currentChatTile.dart
@@ -2,26 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:skly/chat/chatRoom.dart';
 import 'package:skly/controller/chatRoomController.dart';
+import 'package:skly/model/post.dart';
 
 class CurrentChatTile extends StatelessWidget {
-  final String? store;
-  final String? docId;
-  CurrentChatTile(this.store, this.docId);
-  ChatRoomController _chatRoomController = Get.put(ChatRoomController());
+  final Post? post;
+  final int index;
+  CurrentChatTile({required this.post, required this.index});
 
   @override
   Widget build(BuildContext context) {
-    print(docId);
+    ChatRoomController _chatRoomController = Get.put(ChatRoomController(post));
     return ListTile(
-        title: Text(store!),
+        title: Text(post!.store!),
         onTap: () async {
-          print(docId);
-          _chatRoomController.setCurrentRoomId(docId!);
-          print("Tapped:" + _chatRoomController.currentRoomId!);
+          _chatRoomController.setCurrentRoomId(post!.id!);
           await Get.to(ChatRoomPage(
-            chatRoomController: _chatRoomController,
-            docId: docId,
-          ));
+              chatRoomController: _chatRoomController,
+              docId: post!.id!,
+              index: index,
+              post: post));
         });
   }
 }
